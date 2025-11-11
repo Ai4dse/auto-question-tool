6ec473514e90ea56064c5e2d189f1c68ba3352c3
goodguyerik <macbook@edr12876.dip.tu-dresden.de>
2025-11-11 14:31:11 +0100

    modify hungarian method

--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -15,6 +15,7 @@ export default function Library() {
     { id: "kmeans", title: "K-Means Clustering", desc: "Cluster data points into groups." },
     { id: "dbscan", title: "DBSCAN", desc: "Practice the DBSCAN algorithm." },
     { id: "addition", title: "Simple Addition", desc: "Practice simple arithmetic." },
+    { id: "hungarian_method", title: "Hungarian Method", desc: "Practice thh Hungarian Method"}
   ];
 
   const generateRandomSeed = () => Math.floor(Math.random() * 1_000_000).toString();
